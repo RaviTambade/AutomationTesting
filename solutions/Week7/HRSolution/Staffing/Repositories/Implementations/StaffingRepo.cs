e97beef43bf72227087cc00f0f459fbce56d1b61
Ravi Tambade <ravi.tambade@transflower.in>
2025-08-03 10:10:51 +0530

    HR Solution

---
+++ solutions/Week7/HRSolution/Staffing/Repositories/Implementations/StaffingRepo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using  MySql.Data.MySqlClient;
+
+using Staffing;
+using Staffing.Entities;
+
+namespace Staffing.Repositories.Implementations
+{
+    public class StaffingRepo : Interfaces.IStaffingRepo
+    {
+        public bool AddEmployee(Employee employee)
+        {
+            bool status = false;
+
+
+            return status;
+
+        }
+
+        public bool DeleteEmployee(int id)
+        {
+            bool status = false;
+
+
+            return status;
+
+        }
+
+        public List<Employee> GetAllEmployees()
+        { 
+            List<Employee> employees = new List<Employee>();
+             return employees;
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            Employee employee = new Employee();
+            // Logic to retrieve employee by id
+            return employee;
+
+        }
+
+        public bool UpdateEmployee(Employee employee)
+        {
+            bool status = false;
+
+
+            return status;
+
+        }
+    }
+}
---
+++ solutions/Week7/HRSolution/Staffing/Repositories/Interfaces/IStaffingRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Staffing.Entities;
+namespace Staffing.Repositories.Interfaces
+{
+    public  interface IStaffingRepo
+    {
+
+        //CRUD Operations for Employee
+        List<Employee> GetAllEmployees();
+        Employee GetEmployeeById(int id);
+        bool AddEmployee(Employee employee);
+        bool UpdateEmployee(Employee employee);
+        bool DeleteEmployee(int id);
+    }
+}
